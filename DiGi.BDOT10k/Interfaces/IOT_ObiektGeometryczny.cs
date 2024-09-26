7454bc5d174c3310a6fd12f84a37a5f9fab0e858
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-09-26 20:42:43 +0200

    IOT_ObiektGeometryczny Updated

--- DiGi.BDOT10k/Interfaces/IOT_ObiektGeometryczny.cs
+++ DiGi.BDOT10k/Interfaces/IOT_ObiektGeometryczny.cs
@@ -2,7 +2,12 @@
 
 namespace DiGi.BDOT10k.Interfaces
 {
-    public interface IOT_ObiektGeometryczny<T> : GML.Interfaces.IAbstractGML where T : GeometricPrimitive
+    public interface IOT_ObiektGeometryczny : GML.Interfaces.IAbstractGML
+    {
+
+    }
+
+    public interface IOT_ObiektGeometryczny<T> : IOT_ObiektGeometryczny where T : GeometricPrimitive
     {
         public T geometria { get; set; }
     }
