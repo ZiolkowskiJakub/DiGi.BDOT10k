ab264431ce866c3228aeacc104651999622aa0a9
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-03-03 21:49:02 +0100

    OT_ADJA_A implemented

---
+++ DiGi.BDOT10k/Classes/OT_ADJA_A.cs
@@ -0,0 +1,24 @@
+﻿using DiGi.BDOT10k.Enums;
+using DiGi.BDOT10k.Interfaces;
+using DiGi.GML.Classes;
+
+namespace DiGi.BDOT10k.Classes
+{
+    public class OT_ADJA_A : OT_PodzialTerytorialny, IOT_PowierzchniowyObiektGeometryczny
+    {
+        public string identyfikatorPRG { get; set; }
+
+        public string identyfikatorTERYTjednostki { get; set; }
+
+        public string? idTERYTjednostkiNadrzednej { get; set; }
+
+        public OT_RodzajJednostkiAdministracyjnej rodzaj { get; set; }
+
+        public Surface geometria{ get; set; }
+
+        public OT_ADJA_A() 
+        {
+            
+        }
+    }
+}
---
+++ DiGi.BDOT10k/Enums/OT_RodzajJednostkiAdministracyjnej.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DiGi.BDOT10k.Enums
+{
+    public enum OT_RodzajJednostkiAdministracyjnej
+    {
+        [Description("państwo")] panstwo,
+        [Description("województwo")] wojewodztwo,
+        [Description("powiat")] powiat,
+        [Description("gmina")] gmina,
+        [Description("miasto w gminie miejsko-wiejskiej")] miasto_w_gminie_miejsko_wiejskiej,
+        [Description("dzielnica lub delegatura")] dzielnica_lub_delegatura,
+    }
+}
